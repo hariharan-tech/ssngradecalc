32439e5cc7fad20941bd47bafd649d15926c88e6
Hariharan Ananth <72250173+hariharan-tech@users.noreply.github.com>
2021-10-18 15:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,28 +6,28 @@ app = Flask(__name__)
 def calculategpa():
     if request.method=='POST':
         rd={
-            "eng":[request.form['eng'],3],
-            "ac":[request.form['ac'],4],
-            "uph":[request.form['uph'],3],
-            "ucy":[request.form['ucy'],3],
-            "pyt":[request.form['pyt'],3],
-            "eg":[request.form['eg'],3],
-            "ppl":[request.form['ppl'],1.5],
-            "pcl":[request.form['pcl'],1.5]
+            "te":[request.form['te'],3],
+            "cflt":[request.form['cflt'],4],
+            "pee":[request.form['pee'],3],
+            "eie":[request.form['eie'],3],
+            "ed":[request.form['ed'],3],
+            "ca":[request.form['ca'],4],
+            "dp":[request.form['dp'],1.5],
+            "cdl":[request.form['cdl'],1.5]
         }
         # rf={"o":10,"a+":9,"a":8,"b+":7,"b":6}
         for i in rd:
             if rd[i][0] == "0":
                 return render_template("index.html",alertext="Select the Options correctly!")
             if rd[i][0] in ["ra","sa","w","ab","au"]:
-                return render_template('results.html',grade="Sorry bro you have failed! Dont worry do well next time!")
+                return render_template('results.html',grade="Dont worry, do well next time!")
         
         grade=0
         for i in rd:
             grade+=(int(rd[i][0])*rd[i][1])
 #             print(grade)
 
-        grade=round(float(grade/22),1)
+        grade=round(float(grade/23),1)
         return render_template('results.html',grade=grade)
     else:
         return render_template("index.html")
